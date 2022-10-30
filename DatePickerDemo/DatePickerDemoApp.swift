82b5cec32e6d08fa306f15c5dbea09a7b2dfa3f7
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-10-30 22:56:39 +0600

    Initial Commit

---
+++ DatePickerDemo/DatePickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerDemoApp.swift
+//  DatePickerDemo
+//
+//  Created by JOYNAL ABEDIN on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
